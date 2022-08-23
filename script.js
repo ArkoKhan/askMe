50ff2588e7c8db78ac97969f3c312f9adccd68db
ArkoKhan <hasin.arko@gmail.com>
2022-08-24 02:52:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const mainTitle = document.querySelector(".main__sec01__h2");
+const mainInfo = document.querySelector(".main__sec01__p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const result = document.querySelector(".result");
+const resetBtn = document.querySelector(".resetbtn");
+
+
+btn.addEventListener("click", function ()  {
+    let number = parseInt(Math.random() * 6) + 1;
+    mainTitle.style.paddingTop = "10px";
+    mainInfo.style.paddingTop = "10px";
+    input.style.marginTop = "10px";
+    btn.style.marginTop = "10px";
+    result.innerHTML = `<h2>Q: ${input.value}</h2> <p>${number}</p>`;
+
+
+    if (number === 1 && input.value !== "") {
+        result.innerHTML = `<h2>Q: ${input.value}</h2> <p>YES</p>`;
+    } else if (number === 2 && input.value !== "") {
+        result.innerHTML = `<h2>Q: ${input.value}</h2> <p>NO</p>`;
+    } else if (number === 3 && input.value !== "") {
+        result.innerHTML = `<h2>Q: ${input.value}</h2> <p>POSITIVE</p>`;
+    } else if (number === 4 && input.value !== "") {
+        result.innerHTML = `<h2>Q: ${input.value}</h2> <p>NEGATIVE</p>`;
+    } else if (number === 5 && input.value !== "") {
+        result.innerHTML = `<h2>Q: ${input.value}</h2> <p>ABSOLUTELY!</p>`;
+    } else if (number === 6 && input.value !== "") {
+        result.innerHTML = `<h2>Q: ${input.value}</h2> <p>NEVER!</p>`;
+    } else {
+        result.innerHTML = `<h2>Please! Ask a question</h2>`;
+    }
+
+
+    input.value = "";
+});
+
+resetBtn.addEventListener("click", function () {
+    mainTitle.style.paddingTop = "50px";
+    mainInfo.style.paddingTop = "40px";
+    input.style.marginTop = "120px";
+    btn.style.marginTop = "50px";
+    result.innerHTML = "";
+    input.value = "";
+})
